1ee3c057e4d01ff45014e704081f80981cc97aee
Joksanmj72 <joksanmj.777@gmail.com>
2024-03-23 22:18:16 -0600

    ImageGallery with Masonry Layout component, and new Scroll

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,62 +1,88 @@
 "use client";
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo, useRef } from 'react';
 import Image from 'next/image';
+import Masonry from 'react-masonry-css';
 
 interface ImageData {
   id: number;
   webformatURL: string;
 }
-const ImageGallery  = () => {
+
+const ImageGallery = () => {
   const [images, setImages] = useState<ImageData[]>([]);
   const [page, setPage] = useState(1);
+  const [scrollCount, setScrollCount] = useState(0); 
+  const [maxScrolls, setMaxScrolls] = useState(20);
+  const imageGalleryRef = useRef<HTMLDivElement>(null); 
 
-    useEffect(() => {
-      fetchImages();
-    }, [page]); 
-    
-    const fetchImages = async () => {
-      const apiKey = '43003817-5333e0e4f3c66f6765e525f97';
-      const perPage = 15;
-      //const random = Math.floor(Math.random()*10); Random images (but a problem).
-      const apiURL = `https://pixabay.com/api/?key=${apiKey}&orientation=vetical&per_page=${perPage}`;
+  useEffect(() => {
+    fetchImages();
+  }, [ page]);
 
-      try {
-        const response = await fetch(apiURL);
-        const data = await response.json();
-        setImages(prevImages => [...prevImages, ...data.hits]);
-      } catch (error) {
-        console.error('Error al obtener las imágenes:', error);
-      }
-    };
+  const fetchImages = async () => {
+    if (scrollCount >= maxScrolls) return;
+    const apiURL = `https://pixabay.com/api/?key=43003817-5333e0e4f3c66f6765e525f97&orientation=vetical&per_page=20&page=${page}`;
 
-  const lastImage = useMemo(() => images[images.length - 1], [images]);
+    try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      setImages(prevImages => [...prevImages, ...data.hits]);
+    } catch (error) {
+      console.error('ERROR GETTING IMAGES', error);
+    }
+  };
+
+  const lastImages = useMemo(() => images, [images]);
 
   const handleScroll = () => {
-    if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+    const { current } = imageGalleryRef;
+    if (
+      current &&
+      current.scrollHeight - current.scrollTop <= current.clientHeight + 50
+    ) {
       setPage(prevPage => prevPage + 1);
+      setScrollCount(prevScrollCount => prevScrollCount + 1);
     }
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    const { current } = imageGalleryRef;
+    if (current) {
+      current.addEventListener('scroll', handleScroll);
+      return () => current.removeEventListener('scroll', handleScroll);
+    }
+  }, []); 
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
   }, []);
 
   return (
-    <div>
-      <div className="columns-4 gap-5 w-auto mx-auto space-y-3 pb-28">
-        {images.map(image => (
-          <div key={image.id} className='rounded-lg overflow-hidden hover:opacity-50 transition-opacity duration-300'>
+    <div ref={imageGalleryRef} style={{ overflowY: 'auto', maxHeight: '600px' }}>
+       <Masonry
+       breakpointCols={{ 1100: 4, 800: 3,500: 2 }}
+       className="my-masonry-grid"
+       columnClassName="my-masonry-grid_column m-5"
+       style={{ display: 'flex', justifyContent: 'center' }}
+    >
+        {lastImages.map(image => (
+          <div key={image.id} className='masonry-item'>
             <Image
               src={image.webformatURL}
-              alt={`Image ${image.id}`} 
-              width={400}
+              alt={`Image ${image.id}`}
+              width={600}
               height={400}
+              className='rounded-lg overflow-hidden hover:opacity-50 transition-opacity duration-300 m-2'
             />
           </div>
         ))}
-      </div>
+      </Masonry>
+
     </div>
   );
-}  
+};
+
 export default ImageGallery;
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,9 +5,9 @@ import NavOptions from './NavOptions'
 const NavBar = () => {
   const options = ['Load', 'View Images']; 
   return (
-    <div className='p-5 mb-8'>
-    <nav className='rounded-lg navbar font-semibold  text-sm shadow-lg shadow-indigo-500/50 font-mono  flex justify-between'>
-    <label className='text-lg px-5'>Random Infinite Images</label>
+    <div className=' mb-3'>
+    <nav className='rounded-lg navbar font-semibold  text-xs shadow-lg shadow-indigo-500/50 font-mono  flex justify-between'>
+    <label className='text-xs px-5'>Random Infinite Images</label>
     <NavOptions options={options}/>
     </nav>
     </div>
